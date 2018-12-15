cccb542deefff8c3926fe4e122964854d01e90ea
nsawek <snowacoll@hotmail.com>
2018-12-15 21:18:52 +0100

    Add support database

---
+++ Baza/AddBase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace Baza
+{
+    class AddBase
+    {
+        public static void Main()
+        {
+            
+            string connStr = "server=mysql32.mydevil.net;port=3306;database=m11808_baz;user=m11808_wed;password=Xamarin1@#";
+            MySqlConnection conn = new MySqlConnection(connStr);
+            try
+            {
+                string sql = "insert into User(numerkarty,haslo) values(@user,@pass)";
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                cmd.ExecuteNonQuery();
+                // Perform database operations
+            }
+            catch (Exception)
+            {
+
+                //tvTips.Text = "Zalogowano pomyślnie";
+            }
+            conn.Close();
+        }
+        
+    }
+}
--- START/MainActivity.cs
+++ START/MainActivity.cs
@@ -3,17 +3,73 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
+using System.Data;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System;
 
 namespace START
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+
+
+        private EditText etusername;
+        private EditText etpass;
+        private Button btninsert;
+        private TextView tvTips;
+        
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-            // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            etusername = FindViewById<EditText>(Resource.Id.etusername);
+            etpass = FindViewById<EditText>(Resource.Id.etPass);
+            btninsert = FindViewById<Button>(Resource.Id.btninsert);
+            tvTips = FindViewById<TextView>(Resource.Id.tvTips);
+            
+            btninsert.Click += Btninsert_Click;
         }
+
+        private void Btninsert_Click(object sender, System.EventArgs e)
+        {
+            int numerkart = Int32.Parse(etusername.Text);
+            int pass2 = Int32.Parse(etpass.Text);
+            InsertInfo(numerkart, pass2);
+           
+
+        }
+
+        void InsertInfo(int userPar, int passPar)
+        {
+            string connStr = "";
+            MySqlConnection conn = new MySqlConnection(connStr);
+            try
+            {
+                conn.Open();
+                tvTips.Text = "Połączono pomyślnie";
+                MySqlCommand cmd = new MySqlCommand("insert into User(numerkarty,haslo) values(@user,@pass)", conn);
+                cmd.Parameters.AddWithValue("@user", userPar);
+                cmd.Parameters.AddWithValue("@pass", passPar);
+                cmd.ExecuteNonQuery();
+                tvTips.Text = "Zalogowano pomyślnie";
+                
+            }
+            catch (Exception ex)
+            {
+                tvTips.Text = ex.ToString();
+            }
+            finally
+            {
+                conn.Close();
+            }
+            
+                
+            
+        }
+
     }
 }
