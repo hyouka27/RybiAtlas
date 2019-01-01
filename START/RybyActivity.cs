cc2ab43d2258697c70c76f084cd1a3d70e2dfdde
nsawek <snowacoll@hotmail.com>
2019-01-01 20:25:49 +0100

    Mała poprawka

--- START/RybyActivity.cs
+++ START/RybyActivity.cs
@@ -19,22 +19,29 @@ namespace START
     public class RybyActivity : AppCompatActivity
     {
         private TextView NazwaRyby1;
-        //private TextView Indeks1;
-        //private TextView UrlObrazka1;
+        private TextView Indeks1;
+        private TextView UrlObrazka1;
         private TextView Opis1;
         public string Nazwaryby { get; set; }
         public string Opis { get; set; }
+        public string Indeks { get; set; }
+        public string Url_Obrazka { get; set; }
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.activity_ryby);
             NazwaRyby1 = FindViewById<TextView>(Resource.Id.NazwaRyby1);
-            //Indeks1 = FindViewById<TextView>(Resource.Id.Indeks1);
-            //UrlObrazka1 = FindViewById<TextView>(Resource.Id.UrlObrazka1);
+            Indeks1 = FindViewById<TextView>(Resource.Id.Indeks1);
+            UrlObrazka1 = FindViewById<TextView>(Resource.Id.Obrazek);
             Opis1 = FindViewById<TextView>(Resource.Id.Opis1);
             using (var webClient = new System.Net.WebClient())
             {
                 var json = webClient.DownloadString("http://hyouka27.usermd.net/WENDKA/ryby.json");
+                JObject rybki = JObject.Parse(json);
+                NazwaRyby1.Text = (string)rybki["ryby"][0]["NazwaRyby"];
+                Opis1.Text = (string)rybki["ryby"][0]["Opis"];
+                Indeks1.Text = (string)rybki["ryby"][0]["Indeks"];
+                UrlObrazka1.Text = (string)rybki["ryby"][0]["Url_Obrazka"];
             }
             
         } 
