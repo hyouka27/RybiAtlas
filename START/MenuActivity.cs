784e2a32147dbff69c1fa1525643699c98aaa0b1
nsawek <snowacoll@hotmail.com>
2019-01-10 20:56:30 +0100

    poprawki.

--- START/MenuActivity.cs
+++ START/MenuActivity.cs
@@ -77,6 +77,7 @@ namespace START
                 
             };
         }
+
         /// <summary>
         /// Metody odpowiedzialne za działanie przycisków w menu, przekierowują do wybranych aktywnośći.
         /// </summary>
